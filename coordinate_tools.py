7e23c509dadf797a9325437b80957ab41f5e9ae6
David <56723830+dmholtz@users.noreply.github.com>
2020-03-17 21:39:57 +0100

    added coordinate transformation

---
+++ coordinate_tools.py
@@ -0,0 +1,139 @@
+import numpy as np
+
+class Transformation():
+    """Handles cartesian coordinate transformations.
+
+    """
+
+    def __init__(self, rotation_matrix, translation_vector, \
+        check_linear_algebra = True, calc_inverse = True):
+        """Initializes a coordinate transformation object.
+
+        Accepts a rotation matrix and a translation vector, by which a linear
+        coordinate transformation is defined. Calculates the the inverse
+        transformation function and stores it as a CoordinateTransformation
+        object.
+
+        Args:
+            * rotation_matrix (numpy.matrix): a real 3 by 3 orthogonal matrix
+            * translation_vector (numpy.array): a real 3 by 1 column vector
+            * check_linear_algebra (bool): (optional) enable / disable linear
+            algebra check
+            * calc_inverse (bool): (optional) automatically calculate an
+            inverse transformation for this transformation
+
+        Raises:
+            * ValueError: If either translation_vector or rotation_matrix are
+            not 3-dimensional
+            * LinearAlgebraError: If rotation_matrix is not a orthogonal 3 by 3
+            matrix
+        """
+
+        if not Transformation.valid_vector(translation_vector):
+            raise ValueError('Translation vector must be a 3x1 numpy \
+                                array')
+        if not Transformation.valid_matrix(rotation_matrix):
+            raise ValueError('Rotation matrix must be a 3x3 numpy matrix')
+
+        if check_linear_algebra:
+            if not Transformation.orthogonal_matrix(rotation_matrix):
+                raise LinearAlgebraError('Rotation matrix is not orthogonal \
+                    and can thus not represent a cartesian coordinate system')
+
+        self.__rotation_matrix = rotation_matrix
+        self.__translation_vector = translation_vector
+
+        if calc_inverse:
+            self.__inverse_transformation = self._calc_inverse_transformation()
+
+    def transform(self, affine_vector):
+        """Transforms an affine vector to base coordinates.
+
+        Args:
+            * affine_vecotr (numpy.array): a real 3 by 1 column vector
+
+        Raises:
+            * ValueError: If affine_vector is not 3-dimensional
+
+        """
+        
+        if not Transformation.valid_vector(affine_vector):
+            raise ValueError('Affine vector must be a 3x1 dimensonal numpy \
+                array')
+        
+        return np.dot(self.__rotation_matrix, affine_vector) \
+            + self.__translation_vector
+
+
+    def retransform(self, base_vector):
+        """Transforms an base vector to affine coordinates.
+
+        Args:
+            * base_vector (numpy.array): a real 3 by 1 column vector
+
+        Raises:
+            * ValueError: If base_vector is not 3-dimensional
+
+        """
+
+        if not Transformation.valid_vector(base_vector):
+            raise ValueError('Base vector must be a 3x1 dimensonal numpy \
+                array')
+     
+        return self.__inverse_transformation.transform(base_vector)    
+
+    def _calc_inverse_transformation(self):
+        """Calculates the inverse transformation function of this object.
+
+        """
+
+        inverse_matrix = np.linalg.inv(self.__rotation_matrix)
+        inverse_vector = -np.dot(inverse_matrix, self.__translation_vector)
+
+        return Transformation(inverse_matrix, inverse_vector, \
+            calc_inverse=False)
+        
+    @staticmethod
+    def valid_vector(vector):
+        """Returns true if vector is 3 by 1 dimensional and false otherwise.
+
+        Args:
+            * vector (numpy.array)
+
+        """
+        return (vector.shape == (3,)) | (vector.shape == (3,1))
+
+    @staticmethod
+    def valid_matrix(matrix):
+        """Returns true if matrix is 3 by 3 dimensional and false otherwise.
+
+        Args:
+            * matrix (numpy.matrix)
+
+        """
+        return matrix.shape == (3,3)
+    
+    @staticmethod
+    def orthogonal_matrix(matrix):
+        """Returns true if matrix orthogonal and false otherwise.
+
+        A matrix A is orthogonal if and only if A multiplied by its transpose
+        is an identity matrix.
+
+        Args:
+            * matrix (numpy.matrix)
+
+        """
+
+        return np.allclose(np.dot(matrix, np.transpose(matrix)), np.eye(3))
+
+class LinearAlgebraError(Exception):
+    """Exception raised for invalid linear algebra operations related to 
+    transformation process of cartesian cooridinate systems.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
---
+++ kinematics.py
@@ -0,0 +1,17 @@
+class Kinematics():
+    """Represents a robotic arm's kinematics
+
+    """
+
+    def __init__(self):
+        pass
+
+    def forward(self, angles):
+        pass
+
+    def inverse(self, to, end_effecotor_COS):
+        pass
+
+    def set_end_effector(self):
+        pass
+
